b7bb02dcdebaa42d2c2313708add1eac6cb21df0
Abel de Beer <abel@cookingfox.nl>
2016-08-09 11:49:54 +0200

    Adds provided dependency on support v4 lib to attempt to fix Travis javadoc build

--- app-lifecycle/src/main/java/com/cookingfox/android/app_lifecycle/api/listener/AppLifecycleListener.java
+++ app-lifecycle/src/main/java/com/cookingfox/android/app_lifecycle/api/listener/AppLifecycleListener.java
@@ -1,12 +1,5 @@
 package com.cookingfox.android.app_lifecycle.api.listener;
 
-import com.cookingfox.android.app_lifecycle.api.listener.OnAppCreated;
-import com.cookingfox.android.app_lifecycle.api.listener.OnAppFinished;
-import com.cookingfox.android.app_lifecycle.api.listener.OnAppPaused;
-import com.cookingfox.android.app_lifecycle.api.listener.OnAppResumed;
-import com.cookingfox.android.app_lifecycle.api.listener.OnAppStarted;
-import com.cookingfox.android.app_lifecycle.api.listener.OnAppStopped;
-
 /**
  * Listener for all app lifecycle events.
  */
@@ -17,5 +10,4 @@ public interface AppLifecycleListener extends
         OnAppPaused,
         OnAppStopped,
         OnAppFinished {
-
 }
